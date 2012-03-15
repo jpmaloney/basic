340237d952858fdd1b7763c3c48f04e462b857a9
John Maloney <john@rawbw.com>
2012-03-15 01:07:28 -0700

    Confirm Boost header files work.

---
+++ tests/boost_example.cpp
@@ -0,0 +1,25 @@
+//
+// To build:
+//   $ cd ~/basic/tests
+//   $ g++ -I ~/thirdparty/boost boost_example.cpp -o boost_example
+//
+// To run:
+//   $ echo 1 2 3 | ./example
+//
+// Reference:
+// http://www.boost.org/doc/libs/1_49_0/more/getting_started/unix-variants.html
+//
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
+
