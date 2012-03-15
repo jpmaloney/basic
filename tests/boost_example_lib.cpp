8137335c0d7f031b6de5d8a859c0bf514ebdfd97
John Maloney <john@rawbw.com>
2012-03-15 02:50:18 -0700

    Confirm linking and running with Boost binary.

---
+++ tests/boost_example_lib.cpp
@@ -0,0 +1,30 @@
+//
+// To build:
+//     $ g++ -I ~/thirdparty/boost \
+//         boost_example_lib.cpp -o boost_example_lib \
+//         ~/thirdparty/boost/stage/lib/libboost_regex.a
+//
+// To run:
+//     $ ./boost_example_lib
+//
+// Reference:
+// http://www.boost.org/doc/libs/1_49_0/more/getting_started/unix-variants.html#id24
+//
+#include <boost/regex.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string line;
+    boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
+
+    while (std::cin)
+    {
+        std::getline(std::cin, line);
+        boost::smatch matches;
+        if (boost::regex_match(line, matches, pat))
+            std::cout << matches[2] << std::endl;
+    }
+}
+
